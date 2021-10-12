3ebb34b24a2b9828527ab6f53ec2587ec765afbd
Alex-LNB <alecithow@comunidad.unam.mx>
2021-10-12 00:17:37 -0500

    3er comit, modificacion de act1 ya probada la app

--- actividad1/actividad_1/lib/act1.dart
+++ actividad1/actividad_1/lib/act1.dart
@@ -1,5 +1,4 @@
-
-// ignore_for_file: prefer_const_constructors
+// ignore_for_file: prefer_const_constructors, use_key_in_widget_constructors
 
 import 'package:flutter/material.dart';
 
@@ -18,63 +17,73 @@ class Act1 extends StatelessWidget {
         children: [
           Expanded(
             flex: 4,
-            child: Center(
-              child: Transform.rotate(
-                angle: 3.14192/4,
-                child: Container(
-                height: 250,
-                width: 250,
-                padding: EdgeInsets.all(10),
-                decoration: BoxDecoration(
-                  shape: BoxShape.rectangle,
-                  color: Colors.redAccent,
-                ),
-                child: Center(
-
-
-                  child: Transform.rotate(
-                    angle: -3.14192/4,
-                    child: Container(
-                      //height: .1,
-                      //width: .1,
-                      decoration: BoxDecoration(
-                        //borderRadius: BorderRadius.circular(400),
-                        shape: BoxShape.circle,
-                        color: Colors.greenAccent,
-                      ),
-
-                      child: Column(
-                        // ignore: prefer_const_literals_to_create_immutables
-                        children: [
-                          Expanded(child: Center(child: Text("Alejandro Manuel\nHernández García", style: TextStyle(fontSize: 20)),),),
-                          /*Expanded(child: Center(child: Text("Alejandro"),),),
-                          Expanded(child: Center(child: Text("Manuel"),),),
-                          Expanded(child: Center(child: Text("Hernandez"),),),
-                          Expanded(child: Center(child: Text("Garcia"),),),*/
-                        ],
-                      ),
-
-                    ),
-                  ),
-
-
-
-
-                ),
-                ),
-              ),
-            ),
+            child: transformRombo(),
           ),
           Expanded(
-            child: Center(
-              child: Text(
-                "Actividad no. 1",
-                style: TextStyle(fontStyle: FontStyle.italic, fontSize: 25,),
-              ),
-            ),
+            child: textoInferior(),
           ),
         ],
       )
+    );
+  }
+
+  Widget transformRombo(){
+    return Center(
+      child: Transform.rotate(
+        angle: 3.14192/4,
+        child: Container(
+          height: 250,
+          width: 250,
+          padding: EdgeInsets.all(10),
+          decoration: BoxDecoration(
+            shape: BoxShape.rectangle,
+            color: Colors.redAccent,
+          ),
+          child: Center(
+            child: Transform.rotate(
+              angle: -3.14192/4,
+              child: containerCirculo(),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget containerCirculo(){
+    return Container(
+      decoration: BoxDecoration(
+        shape: BoxShape.circle,
+        color: Colors.greenAccent,
+      ),
+      child: Expanded(
+        child: textoCirculo(),
+      ),
+    );
+  }
+
+  Widget textoCirculo(){
+    return Center(
+      child: Text(
+        "Alejandro Manuel\nHernández García",
+        style: TextStyle(
+          fontSize: 20,
+          color: Colors.grey[800],
+        ),
+      ),
+    );
+  }
+
+  Widget textoInferior(){
+    return Center(
+      child: Text(
+        "Actividad no. 1",
+        style: TextStyle(
+          fontStyle: FontStyle.italic,
+          fontSize: 25,
+          color: Colors.grey[800],
+        ),
+      ),
     );
   }
 
